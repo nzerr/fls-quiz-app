467e1ad7f7402ce8c3b68c79b1fae69f43302283
nzerr <nicolas.zerr@gmail.com>
2019-03-18 21:35:43 +0100

    Updates

---
+++ precache-manifest.949f71b5ad64a7bf075ddbf488b4607c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e6a937045a7c8d8dcd938c0ea44dfd",
+    "url": "/fls-quiz-app/static/media/quantum01.59e6a937.jpg"
+  },
+  {
+    "revision": "3367c6c16db9f8dfe0cc5bd7d39ca634",
+    "url": "/fls-quiz-app/static/media/logo.3367c6c1.svg"
+  },
+  {
+    "revision": "2937ce3952eb92bb06c8",
+    "url": "/fls-quiz-app/static/js/runtime~main.4bb6f064.js"
+  },
+  {
+    "revision": "8d9451379de25b2204ac",
+    "url": "/fls-quiz-app/static/js/main.b43fad81.chunk.js"
+  },
+  {
+    "revision": "b212018de3ed1315cb99",
+    "url": "/fls-quiz-app/static/js/2.2788dbeb.chunk.js"
+  },
+  {
+    "revision": "8d9451379de25b2204ac",
+    "url": "/fls-quiz-app/static/css/main.a6658cac.chunk.css"
+  },
+  {
+    "revision": "61eb40f6a19a30a86f0a3054257a7b14",
+    "url": "/fls-quiz-app/index.html"
+  }
+];
